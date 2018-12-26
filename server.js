b730b9a28b5a3b8a3586c587b1c3c5c85443aa04
abidkhan880 <khanabid844708@gmail.com>
2018-12-26 15:44:38 +0530

    hh

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
 const ejs = require('ejs');
-const mysql = require('mysql');
+//const mysql = require('mysql');
 
 // const db = mysql.createConnection({
 //     host: "localhost",
